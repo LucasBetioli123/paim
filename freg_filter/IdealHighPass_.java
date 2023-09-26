ee116a8e9f90bbdcfc5e04af2efee35e2f2f501d
Lucas Betioli <lucasantoniobetioli@gmail.com>
2023-09-26 02:16:33 -0300

    para funcionar no imaj

--- freg_filter/IdealHighPass_.java
+++ freg_filter/IdealHighPass_.java
@@ -23,7 +23,7 @@ public class IdealHighPass_ extends PlugInFrame implements ActionListener {
 			return;
 		}
 		instance = this;
-		IJ.register(LowPass_.class);
+		IJ.register(IdealHighPass_.class);
 
 		setLayout(new FlowLayout());
 		panel = new Panel();
